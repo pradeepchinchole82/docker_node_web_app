fa80d40c93f64f01346f2d7853f64f62f27d957e
Pradeep Chinchole <dpchinchole@gmail.com>
2022-06-05 12:03:59 +0000

    second commit

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/welcome', (req, res) => {
+    res.send('Welcome To Docker Git Jenkins Integration');
+});
+
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
-
-
-
-
+console.log(`Running on http://${HOST}:${PORT}`);                                                                            
